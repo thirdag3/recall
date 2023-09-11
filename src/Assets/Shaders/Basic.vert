c5266facb849d1010115c3dc58e201ece676760b
Bruno Camargo <bruno.br.camargo@gmail.com>
2023-09-11 16:21:24 -0300

    feat: spinning

--- src/Assets/Shaders/Basic.vert
+++ src/Assets/Shaders/Basic.vert
@@ -16,6 +16,6 @@ out vec3 outColor;
 void main()
 {
     // gl_Position = projection * view * model * vec4(aPos, 1.0);
-    gl_Position = model * vec4(aPos, 1.0);
+    gl_Position = projection * view * model * vec4(aPos, 1.0);
     outColor = aColor;
 }
--- src/Main.cpp
+++ src/Main.cpp
@@ -5,13 +5,13 @@
 #include <memory>
 #include <vector>
 
-#include "Shader.hpp"
 #include "BufferLayout.hpp"
-#include "VertexBuffer.hpp"
 #include "IndexBuffer.hpp"
+#include "Renderer.hpp"
+#include "Shader.hpp"
 #include "UniformBuffer.hpp"
 #include "VertexArray.hpp"
-#include "Renderer.hpp"
+#include "VertexBuffer.hpp"
 
 struct Vertex
 {
@@ -52,6 +52,12 @@ int main(int argc, const char** argv)
     vao.AddVertexBuffer(vbo);
     vao.SetIndexBuffer(std::move(ebo));
 
+    struct ViewProjectionMatrix
+    {
+        glm::mat4 projection;
+        glm::mat4 view;
+    } viewProjection{};
+
     UniformBuffer ubo(128, 0);
 
     Shader s("Assets/Shaders/Basic.vert", "Assets/Shaders/Basic.frag");
@@ -60,14 +66,24 @@ int main(int argc, const char** argv)
     Renderer r;
 
     glm::mat4 transformation(1.0f);
-    transformation =
-        glm::rotate(transformation, glm::radians(180.0f), glm::vec3(1, 0, 0));
+    
 
-    s.SetUniform("model", transformation);
+    viewProjection.projection =
+        glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+
+    viewProjection.view =
+        glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -3.0f));
+
+    ubo.SetData(
+        reinterpret_cast<void*>(&viewProjection), sizeof(viewProjection), 0);
 
     vao.Bind();
     while (!w.ShouldClose()) {
         r.Clear({0.39f, 0.58f, 0.92f, 1.0f});
+
+        transformation = glm::rotate(transformation, 0.03f, glm::vec3(0.0f, 0.2f, 0.0f));
+        s.SetUniform("model", transformation);
+
         r.DrawIndexed(vao);
         w.Update();
     }
