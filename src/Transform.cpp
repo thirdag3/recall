e06ab84a664dde7ca2d2d3a68be5ccfea4aa39ce
Bruno Camargo <bruno.br.camargo@gmail.com>
2023-09-15 02:35:36 -0300

    fix: whoopsie

--- src/Transform.cpp
+++ src/Transform.cpp
@@ -4,8 +4,8 @@
 
 Transform::Transform()
 {
-    m_position = glm::vec3(1.0f);
-    m_rotation = glm::vec3(1.0f);
+    m_position = glm::vec3(0.0f);
+    m_rotation = glm::vec3(0.0f);
     m_scale = glm::vec3(1.0f);
 }
 
