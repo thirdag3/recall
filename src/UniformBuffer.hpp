d3852bb78f7eb7470968cc532713f6170fe84a67
9iv <91408910+9iv@users.noreply.github.com>
2023-09-16 16:07:23 -0300

    feat: add linux stddef header

--- src/UniformBuffer.hpp
+++ src/UniformBuffer.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <glad/glad.h>
+#include <stddef.h>
 
 class UniformBuffer
 {
--- src/VertexAttribute.hpp
+++ src/VertexAttribute.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stddef.h>
+
 struct VertexAttribute
 {
     unsigned int type;
